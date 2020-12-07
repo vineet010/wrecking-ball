494399e80ab3223c3079fe53551014c5a6f2f900
vineet010 <72596306+vineet010@users.noreply.github.com>
2020-12-07 16:04:12 +0530

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,27 @@
+class Ball{
+    constructor(x,y,width,height,angle){
+        var options={
+            
+            'frictionAir':0.005,
+            'density':1
+            
+        }
+
+        this.body=Bodies.rectangle(x,y,width,height,options);
+        this.width=width;
+        this.height=height;
+        World.add(world,this.body)
+
+    }
+    display(){
+        var pos=this.body.position;
+        var angle=this.body.angle;
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        fill("red");
+        ellipse(0,0,this.this.width,this.height)
+       
+        pop();
+    }
+}
